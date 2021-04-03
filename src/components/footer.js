4a07af7053a44b7b485020cb91079d3f2d488bfb
m1sht1 <sanj.mins@gmail.com>
2021-04-03 18:23:15 -0400

    third commit

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,10 +7,10 @@ function Footer(props) {
 			<a href="http://www.twitter.com" target="_blank">
 				<FaTwitter size={40} />
 			</a>
-			<a href="http://www.facebook.com/3cch3" target="_blank">
+			<a href="http://www.facebook.com/sanjana.sharmin.77" target="_blank">
 				<FaFacebook size={40} />
 			</a>
-			<a href="http://www.instagram.com/dashishir" target="_blank">
+			<a href="http://www.instagram.com/sharmin6780" target="_blank">
 				<FaInstagram size={40} />
 			</a>
 			<a href="http://www.linkedin.com/https://www.linkedin.com/in/dasshishir" target="_blank">
